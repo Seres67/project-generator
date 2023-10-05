a98dd6de7676c6e965aad787cdb0260b6a809624
Laurent <haarlaurent67@hotmail.fr>
2023-10-05 02:06:35 +0200

    v0.1

---
+++ include/lang.hpp
@@ -0,0 +1,10 @@
+#ifndef _PROJECT_GENERATOR_LANG_HPP
+#define _PROJECT_GENERATOR_LANG_HPP
+
+enum class Lang {
+    C,
+    CPP,
+    RUST
+};
+
+#endif // !_PROJECT_GENERATOR_LANG_HPP
---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include "include/lang.hpp"
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <unordered_set>
+
+std::string add_leading_slash(const std::string &name)
+{
+    if (name.back() == '/')
+        return name;
+    return std::string(name + "/");
+}
+
+void setup_directories(const std::string &name)
+{
+    std::filesystem::create_directories(name);
+    std::filesystem::create_directory(name + "/include");
+    std::filesystem::create_directory(name + "/src");
+    std::cout << "Created project, source and header directories!" << std::endl;
+}
+
+void setup_main_c(const std::string &name)
+{
+    std::ofstream file;
+    file.open("./" + name + "/src/main.c");
+    file << "#include <stdio.h>" << std::endl << std::endl;
+    file << "int main(int ac, char **av)" << std::endl;
+    file << "{" << std::endl;
+    file << "    printf(\"Hello world!\\n\");" << std::endl;
+    file << "    return 0;" << std::endl;
+    file << "}" << std::endl;
+    file.close();
+    std::cout << "Created src/main.c with a hello world!" << std::endl;
+}
+
+void setup_main_cpp(const std::string &name)
+{
+    std::ofstream file;
+    file.open("./" + name + "/src/main.cpp");
+    file << "#include <iostream>" << std::endl << std::endl;
+    file << "int main(int ac, char *av[])" << std::endl;
+    file << "{" << std::endl;
+    file << "    std::cout << \"Hello world!\" << std::endl;" << std::endl;
+    file << "}" << std::endl;
+    file.close();
+    std::cout << "Created src/main.cpp with a hello world!" << std::endl;
+}
+
+void setup_meson(const std::string &name, const std::string &lang)
+{
+    std::ofstream file;
+    file.open("./" + name + "/meson.build");
+    file << "project('" << name << "', '" << lang << "')" << std::endl;
+    file << "src = ['src/main." << lang << "']" << std::endl;
+    file << "executable('" << name << "', src)" << std::endl;
+    file.close();
+    std::cout << "Created meson.build file!" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cout << "USAGE: project-generator <language> <name>" << std::endl;
+    }
+    std::string lang(argv[1]);
+    std::cout << "argv[1] = " << argv[1] << std::endl;
+    std::unordered_set<std::string> langs {"c", "cpp", "c++"};
+    if (!langs.contains(lang)) {
+        std::cout << "Language not supported yet!" << std::endl;
+        return 0;
+    }
+    std::string name(argv[2]);
+    setup_directories(name);
+    if (lang == "c") {
+        setup_main_c(name);
+    } else if (lang == "c++" || lang == "cpp") {
+        setup_main_cpp(name);
+    }
+    setup_meson(name, lang);
+    return 0;
+}
